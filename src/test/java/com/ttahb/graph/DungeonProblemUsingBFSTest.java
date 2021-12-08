c6fcd7f2e206f3cfbaeac282d1a86889977c3119
ttahb <bhattvijay69@hotmail.com>
2021-12-08 18:27:15 +0530

    renamed testcases for dungeon problem

--- src/test/java/com/ttahb/graph/DungeonProblemUsingBFSTest.java
+++ src/test/java/com/ttahb/graph/DungeonProblemUsingBFSTest.java
@@ -5,30 +5,6 @@ import org.junit.jupiter.api.Test;
 
 public class DungeonProblemUsingBFSTest {
 
-    @Test
-    public void testCreateDungeonUsingAdjMatrix(){
-
-        int[][] dungeon = getInts();
-        DungeonProblemUsingBFS dungeonProblem = new DungeonProblemUsingBFS(dungeon,5,7);
-
-        int timeToReach = dungeonProblem.findTheTimeToReachExit(0,0); // starting cell (0,0)
-
-        Assertions.assertEquals(9,timeToReach);
-
-    }
-
-    @Test
-    public void testCreateDungeonUsingAdjMatrix2(){
-
-        int[][] dungeon = getInts();
-        DungeonProblemUsingBFS dungeonProblem = new DungeonProblemUsingBFS(dungeon,5,7);
-
-        int timeToReach = dungeonProblem.findTheTimeToReachExit(2,4); // starting cell (2,4)
-
-        Assertions.assertEquals(3,timeToReach);
-
-    }
-
     /**
      *       dash -  represents empty cell
      *       # - represents cubes filled with rocks
@@ -62,5 +38,31 @@ public class DungeonProblemUsingBFSTest {
 
         return dungeon;
     }
+
+    @Test
+    public void testFindTheTimeToReachExit1(){
+
+        int[][] dungeon = getInts();
+        DungeonProblemUsingBFS dungeonProblem = new DungeonProblemUsingBFS(dungeon,5,7);
+
+        int timeToReach = dungeonProblem.findTheTimeToReachExit(0,0); // starting cell (0,0)
+
+        Assertions.assertEquals(9,timeToReach);
+
+    }
+
+    @Test
+    public void testFindTheTimeToReachExit2(){
+
+        int[][] dungeon = getInts();
+        DungeonProblemUsingBFS dungeonProblem = new DungeonProblemUsingBFS(dungeon,5,7);
+
+        int timeToReach = dungeonProblem.findTheTimeToReachExit(2,4); // starting cell (2,4)
+
+        Assertions.assertEquals(3,timeToReach);
+
+    }
+
+
 
 }
